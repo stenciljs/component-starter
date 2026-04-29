7857b72bb6c21ae106b9e7a139ed6443f3992126
John Jenkins <johnljenkins@hotmail.com>
2026-04-29 12:31:32 +0100

    chore: use `loader` for tests

--- vitest-setup.ts
+++ vitest-setup.ts
@@ -1,4 +1,3 @@
-// Load Stencil components for browser tests
-await import('./dist/stencil-starter-project-name/stencil-starter-project-name.esm.js');
+import { defineCustomElements } from "./loader";
 
-export {};
+defineCustomElements();
